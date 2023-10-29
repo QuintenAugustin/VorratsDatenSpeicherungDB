41698c6c2dc33d0075be1dd53e82d36f80696ccc
QuintenAugustin <92809829+QuintenAugustin@users.noreply.github.com>
2023-10-29 21:58:06 +0100

    Ein bisschen formatierung und weitere infos

--- main.py
+++ main.py
@@ -4,39 +4,35 @@ from datetime import datetime
 import os.path
 import constants
 
+#Does all the main API work. Easier this way because DB documents how to handle Python.S
+
 conn = http.client.HTTPSConnection("apis.deutschebahn.com")
 
+#No looking at my keys. That'd be an amateur mistake
 headers = {
     'DB-Client-Id': constants.API_CLIENT_ID ,
     'DB-Api-Key': constants.API_KEY,
     'accept': "application/xml"
     }
 
-conn.request("GET", "/db-api-marketplace/apis/timetables/v1/fchg/8000105", headers=headers)
+#fetches info for specific train station
+conn.request("GET", "/db-api-marketplace/apis/timetables/v1/fchg/8000028", headers=headers)
 
 
 res = conn.getresponse()
 data = res.read()
 
-
+#Fetches date and time for proper file naming.
 current_datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
 
-#converts datetime object to string
+#converts datetime object to string and defines Filepath
 str_current_datetime = str(current_datetime)
 file_name = str_current_datetime+".xml"
+timetableChangesFilePath = "rawdata/timetableChanges/"+ file_name
 
-
-
-textFilePath = "rawdata/"+ file_name
-
-with open(textFilePath, 'w') as f:
+#Writes timetable changes to file
+with open(timetableChangesFilePath, 'w') as f:
     f.write(data.decode("utf-8"))
     f.close()
 
 
-
-#file = open(completeName,'w')
-#print(completeName)
-#file.write(data.decode("utf-8"))
-#print("File created: ", file.name)
-#file.close()
