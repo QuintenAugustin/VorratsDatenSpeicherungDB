6b731f4b1a5d7ab68c00848c8afc342201e7f106
QuintenAugustin <92809829+QuintenAugustin@users.noreply.github.com>
2023-11-30 21:21:34 +0100

    ok NOW

--- otherStuffThatMadeThisPossible/viewForR2.sql
+++ otherStuffThatMadeThisPossible/viewForR2.sql
@@ -31,4 +31,5 @@ FROM [test].[dbo].[PlannedArrivalsVIEW3] PLAAR
 FULL OUTER JOIN [test].[dbo].PlannedDepartures AS PLADEP ON PLAAR.uniqueId = PLADEP.uniqueId
 LEFT OUTER JOIN [test].[dbo].PlannedTrainInformation AS PLATR ON PLAAR.uniqueId = PLATR.uniqueId
 LEFT OUTER JOIN [test].[dbo].ChangedArrivals AS CHAAR ON PLAAR.plannedDataUniqueId = CHAAR.uniqueId
+/* This last join works just fine because Planned Arrivals always ends up containing all entries (by ID) that departure data contains and i was stupid to split the two in two Tables */
 LEFT OUTER JOIN [test].dbo.ChangedDepartures AS CHADEP ON PLAAR.plannedDataUniqueId = CHADEP.uniqueId
